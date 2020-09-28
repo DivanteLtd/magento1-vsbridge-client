5388debb6e2df45c04a435c9c661b6feb3bd6443
Christopher Wünschmann <cknoetschke@googlemail.com>
2020-09-28 11:08:52 +0200

    Added possibility to configure the url prefix (#14)
    Co-authored-by: Christopher Wünschmann <cknoetschke@gmail.com>

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ module.exports.Magento1Client = function (options) {
 
   let client = RestClient(options);
 
-  instance.user = user(client);
-  instance.cart = cart(client);
-  instance.order = order(client);
-  instance.stock = stock(client);
-  instance.contact = contact(client);
-  instance.wishlist = wishlist(client);
-  instance.stockAlert = stockAlert(client);
-  instance.newsletter = newsletter(client);
-  instance.address = address(client);
-  instance.settings = settings(client);
+  instance.user = user(client, options.user || {});
+  instance.cart = cart(client, options.cart || {});
+  instance.order = order(client, options.order || {});
+  instance.stock = stock(client, options.stock || {});
+  instance.contact = contact(client, options.contact || {});
+  instance.wishlist = wishlist(client, options.wishlist || {});
+  instance.stockAlert = stockAlert(client, options.stockAlert || {});
+  instance.newsletter = newsletter(client, options.newsletter || {});
+  instance.address = address(client, options.address || {});
+  instance.settings = settings(client, options.settings || {});
 
   return instance;
 };
